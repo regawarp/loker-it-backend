6cc32bfd7f57e00ea46e39581957870a85f43811
annazarryuu <annazar.ryuu@gmail.com>
2022-12-05 09:42:32 +0700

    Add post tweet manual function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var captionsRouter = require('./routes/captions');
+var tweetsRouter = require('./routes/tweets');
 var downloadPosterRouter = require('./routes/downloadPosterRouter');
 var listPosterRouter = require('./routes/listPosterRouter');
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/captions', captionsRouter);
+app.use('/tweets', tweetsRouter);
 app.use('/download-poster', downloadPosterRouter);
 app.use('/poster', listPosterRouter);
 
