14dde68728a04170393b51723b3066f2f81be7af
annazarryuu <annazar.ryuu@gmail.com>
2022-12-13 14:31:49 +0700

    Init scheduler to post tweet from database

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,64 +1,6 @@
 var express = require('express');
-var twitter = require('twitter');
 var router = express.Router();
-
-const fs = require('fs');
-const Axios = require('axios');
-
-require("dotenv").config();
-
-const twitterClient = new twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
-
-/* Download an Image from Given URL. */
-async function downloadImage(imageUrl, filepath) {
-    const response = await Axios({
-        url: imageUrl,
-        method: 'GET',
-        responseType: 'stream'
-    });
-    return new Promise((resolve, reject) => {
-        response.data.pipe(fs.createWriteStream(filepath))
-            .on('finish', resolve)
-            .on('error', reject);
-    });
-}
-
-/* Post a Tweet with Given Image and Caption. */
-function postTweet(imagePaths, caption) {
-    let image_ids = []
-    for (let i = 0; i < imagePaths.length; i++) {
-        var image = require('fs').readFileSync(imagePaths[i]);
-        twitterClient.post('media/upload', { media: image }, function (error, media, response) {
-            if (!error) {
-                // console.log(media);
-                image_ids.push(media.media_id_string);
-                if (image_ids.length == imagePaths.length) {
-                    const status = {
-                        status: caption,
-                        media_ids: image_ids.join(','),
-                    }
-                    twitterClient.post('statuses/update', status, function (error, tweet, response) {
-                        if (!error) {
-                            // console.log(tweet);
-                            return null;
-                        }
-                        else {
-                            return error;
-                        }
-                    });
-                }
-            }
-            else {
-                return error;
-            }
-        });
-    }
-}
+const tweets = require('../utility/tweets');
 
 /* Post a Tweet. */
 router.post('/', async function (req, res, next) {
@@ -67,22 +9,7 @@ router.post('/', async function (req, res, next) {
         return;
     }
 
-    let imagePaths = [];
-    for (let i = 0; i < req.body.images.length; i++) {
-        const imagePath = 'posted_images/' + Date.now() + '.jpg';
-        await downloadImage(req.body.images[i], imagePath).then(
-            (result) => {
-                imagePaths.push(imagePath);
-            },
-            (error) => {
-                console.log('Error :', error);
-                res.send('failed!');
-                return;
-            }
-        );
-    }
-
-    const error = postTweet(imagePaths, req.body.caption);
+    const error = await tweets.postTweetFromImageUrl(req.body.images, req.body.caption)
     if (!error) res.send('success!');
     else res.send('failed!');
     return;
--- utility/scheduler.js
+++ utility/scheduler.js
@@ -1,5 +1,9 @@
 const schedule = require("node-schedule");
 const db = require("./postgres");
+const { ScheduledTweets } = require("../utility/scheduledTweets");
+const tweets = require('../utility/tweets');
+
+require("dotenv").config();
 
 async function getScheduledTweets() {
   const query = `select * from tweets where tweet_status = 0`;
@@ -27,7 +31,28 @@ async function getPosters(tweetId) {
 }
 
 async function runScheduler() {
+  console.log('Running scheduler...');
+  console.log('Getting all scheduled tweet...');
 
+  const scheduledTweets = await ScheduledTweets.getScheduledTweets();
+  console.log(scheduledTweets.length, 'scheduled tweets found!');
+
+  for (let i = 0; i < scheduledTweets.length; i++) {
+    const { tweet_id, tweet_caption_text, tweet_scheduled_date, tweet_posters } = scheduledTweets[i];
+    const imageUrl = tweet_posters.map(poster => process.env.BE_URL + poster.poster_image_path)
+    // console.log(tweet_id, tweet_caption_text, tweet_scheduled_date, imageUrl);
+
+    const timeNow = new Date();
+    const timeSchedule = new Date(tweet_scheduled_date);
+    if (timeNow > timeSchedule) continue;
+    console.log('tweet', tweet_id, 'will be posted at', timeSchedule.toString());
+
+    const job = schedule.scheduleJob(tweet_id, timeSchedule, async function () {
+      const error = await tweets.postTweetFromImageUrl(imageUrl, tweet_caption_text);
+      if (!error) console.log('tweet', tweet_id, 'posted!');
+      else console.log('tweet', tweet_id, 'not posted!');
+    });
+  }
 }
 
 module.exports = runScheduler;
